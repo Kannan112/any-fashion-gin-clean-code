d747de0b38801cead5219db3b27b9d881a28af9d
kannan112 <kannangt100@gmail.com>
2023-11-12 23:15:27 +0530

    added refresh token database

---
+++ pkg/domain/token.go
@@ -0,0 +1,15 @@
+package domain
+
+type AdminRefreshToken struct {
+	RefreshTokenID uint   `json:"refresh_token_id" gorm:"primaryKey;unique;not null"`
+	RefreshToken   string `json:"refresh_token"`
+	AdminID        uint
+	Admin          Admin `gorm:"foregineKey:AdmiID"`
+}
+
+type UserRefreshToken struct {
+	RefreshTokenID uint   `json:"refresh_token_id" gorm:"primaryKey;unique;not null"`
+	RefreshToken   string `json:"refresh_token"`
+	UserID         uint
+	Users          Users `gorm:"foregineKey:AdmiID"`
+}
