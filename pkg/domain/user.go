6dce7781a68a4c3d3d29bbc26533f5a76c83c6d9
kannan112 <kannangt100@gmail.com>
2023-11-16 21:28:47 +0530

    added new column user image from callback

--- pkg/domain/user.go
+++ pkg/domain/user.go
@@ -7,6 +7,7 @@ type Users struct {
 	Name      string `json:"name" binding:"required"`
 	Email     string `json:"email" binding:"required,email" gorm:"unique;not null"`
 	Mobile    string `json:"mobile" binding:"required,eq=10" gorm:"unique; not null"`
+	Images    string `json:"images"`
 	Password  string `json:"password" gorm:"not null"`
 	IsBlocked bool   `gorm:"default:false"`
 	CreatedAt time.Time
