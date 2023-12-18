973914bbd5d770e8384891012c77fb88a229f05f
kannan112 <kannangt100@gmail.com>
2023-12-19 03:34:54 +0530

    google auth

---
+++ pkg/domain/goauth.go
@@ -0,0 +1,12 @@
+package domain
+
+type GoAuthUserInfo struct {
+	ID         string `json:"id"`
+	Email      string `json:"email"`
+	Verified   bool   `json:"verified_email"`
+	Name       string `json:"name"`
+	GivenName  string `json:"given_name"`
+	FamilyName string `json:"family_name"`
+	Picture    string `json:"picture"`
+	Locale     string `json:"locale"`
+}
