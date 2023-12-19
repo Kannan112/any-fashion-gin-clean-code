67faa7bfa4cf7758b5c1a4eb7c631779fa3d8f33
kannan112 <kannangt100@gmail.com>
2023-12-19 07:02:18 +0530

    google auth usecase sep

--- pkg/usecase/authUseCase.go
+++ pkg/usecase/authUseCase.go
@@ -4,7 +4,7 @@ import (
 	"context"
 
 	"github.com/kannan112/go-gin-clean-arch/pkg/api/middleware/token"
-	"github.com/kannan112/go-gin-clean-arch/pkg/domain"
+	"github.com/kannan112/go-gin-clean-arch/pkg/common/req"
 	services "github.com/kannan112/go-gin-clean-arch/pkg/usecase/interface"
 
 	interfaces "github.com/kannan112/go-gin-clean-arch/pkg/repository/interface"
@@ -20,16 +20,34 @@ func NewAuthUseCase(Repo interfaces.UserRepository) services.AuthUserCase {
 	}
 }
 
-func (c *AuthUseCase) GoogleLoginUser(ctx context.Context, googleuser domain.Users) (string, string, error) {
-	data, err := c.UserRepo.UserLogin(ctx, googleuser.Email)
+func (c *AuthUseCase) GoogleLoginUser(ctx context.Context, googleuser req.GoogleAuth) (string, string, error) {
+
+	clean, err := c.UserRepo.UserLogin(ctx, googleuser.Email)
 	if err != nil {
 		return "", "", err
 	}
-	AccessTokenString, err := token.JWTAccessTokenGen(int(data.ID), "user")
+	if clean.ID == 0 {
+		data, err := c.UserRepo.AuthSignUp(googleuser)
+		if err != nil {
+			return "", "", err
+		}
+		AccessTokenString, err := token.JWTAccessTokenGen(int(data.Id), "user")
+		if err != nil {
+			return "", "", err
+		}
+		RefreshTokenString, err := token.JWTRefreshTokenGen(int(data.Id), "user")
+		if err != nil {
+			return "", "", err
+		}
+		return AccessTokenString, RefreshTokenString, nil
+
+	}
+
+	AccessTokenString, err := token.JWTAccessTokenGen(int(clean.ID), "user")
 	if err != nil {
 		return "", "", err
 	}
-	RefreshTokenString, err := token.JWTRefreshTokenGen(int(data.ID), "user")
+	RefreshTokenString, err := token.JWTRefreshTokenGen(int(clean.ID), "user")
 	if err != nil {
 		return "", "", err
 	}
--- pkg/usecase/interface/auth.go
+++ pkg/usecase/interface/auth.go
@@ -3,15 +3,9 @@ package interfaces
 import (
 	"context"
 
-	"github.com/kannan112/go-gin-clean-arch/pkg/domain"
+	"github.com/kannan112/go-gin-clean-arch/pkg/common/req"
 )
 
 type AuthUserCase interface {
-	//
-	GoogleLoginUser(ctx context.Context, googleuser domain.Users) (string, string, error)
-	/*
-		pending :
-				AdminUseCase TO AuthUseCase -> admin login,admin signup
-				UserUseCase TO AuthUseCase -> userLogin,userSignup....
-	*/
+	GoogleLoginUser(ctx context.Context, googleuser req.GoogleAuth) (string, string, error)
 }
