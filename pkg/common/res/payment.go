418ef51aebaa7c56ccfa56a879f1dfcb6f38f218
kannan112 <kannangt100@gmail.com>
2024-02-13 19:00:43 +0530

    Modified payment response

--- pkg/common/res/payment.go
+++ pkg/common/res/payment.go
@@ -23,3 +23,15 @@ type OrderPayment struct {
 	PaymentType  domain.PaymentType `json:"payment_type"`
 	PaymentOrder any                `json:"payment_order"`
 }
+
+type RazorpayOrder struct {
+	RazorpayKey     string      `json:"razorpay_key"`
+	UserID          uint        `json:"user_id"`
+	AmountToPay     uint        `json:"amount_to_pay"`
+	RazorpayAmount  uint        `json:"razorpay_amount"`
+	RazorpayOrderID interface{} `json:"razorpay_order_id"`
+	Email           string      `json:"email"`
+	Phone           string      `json:"phone"`
+
+	ShopOrderID uint `json:"shop_order_id"`
+}
